f7a424aed31e8192fa06946d0d475a240e38c8a2
TOS-the-Game <114623689+TOS-the-Game@users.noreply.github.com>
2022-09-28 15:15:18 -0700

    Add files via upload

---
+++ DEMO.bat
@@ -0,0 +1,298 @@
+:menu
+@echo off
+color 80
+title The Other Side
+cls
+echo The Other Side
+echo  - a 2022 small project -
+echo 1. Start
+echo 2. Acts
+echo 3. Credits
+echo 4. Exit
+set /p answer=Type the number of your option and press enter: 
+if %answer%==1 goto Act_1
+if %answer%==2 goto Acts
+if %answer%==3 goto Credits
+if %answer%==4 goto Exit
+
+:Credits
+cls
+echo Credits
+echo.
+echo Thank you for playing The Other Side!
+pause
+goto Menu
+
+:Exit
+cls
+echo Thanks for playing!
+pause
+exit /b
+
+:Acts
+cls
+echo Please type the number of which you wish to see
+echo 1. Act 1- Meeting Kibo
+echo 2. Act 2- Start of a new life
+echo 3. Return to Menu
+set /p answer=Type the number of your option and press enter: 
+if %answer%==1 goto Act_1
+if %answer%==2 goto Act_2
+if %answer%==3 goto Menu
+pause
+
+
+:Act_1
+color 08
+cls
+echo ?: Good morning world!
+pause
+cls
+echo This is Tammy. She is an 18-year-old girl
+echo She lives alone. Her parents are constantly traveling for work.
+echo But today is special.
+pause
+cls
+echo Today is Tammy's birthday.
+pause
+cls
+echo Tammy grabs her cellphone and checks her voicemails.
+pause
+cls
+echo Tammy: Great, not even a voicemail, text, or call 
+echo I wonder if work is that important to them...
+pause
+cls
+echo She dials a number.
+pause
+cls
+echo WS Cafe: Thank you for calling Woodville Seattle cafe,
+echo how may I help you?
+pause
+cls 
+echo Tammy: Are you available to make a reservation for one?  
+echo Preferably 3 hours from now.
+pause
+cls
+echo WS Cafe: Does 9:30 work?
+pause
+cls
+echo Tammy: Yeah.
+pause
+cls
+echo She finishes getting ready and heads out to her college class.
+pause
+cls
+echo Male Student: Hey lady, watch out!
+pause
+cls
+echo She just so happened to get into the crossfire  
+echo of a small milk fight while heading to her classes.
+pause
+cls
+echo Tammy: Im soaked...
+pause
+cls
+echo Male Student 2: Why cant you watch where youre going?
+pause
+cls
+echo Female Student: Haha! Her clothes are soaked!
+pause
+cls
+echo By college you wouldve expect her grade school bullies to mature.
+echo But this became a common occurrence as of lately.
+pause
+cls
+echo Tammy: You.. Idiots.. 
+pause
+cls
+echo Tammy: Why cant you watch where youre throwing stuff! 
+echo Why even through milk to begin with?                  
+echo Youve had more than 4 years to grow up, but instead
+echo Take every opportunity to act-
+pause
+cls
+echo The group began laughing.
+pause
+cls
+echo Male Student: What? You take everything so seriously! 
+pause
+cls
+echo Female Student: She looks like shes about to cry!
+pause
+cls
+echo Tammy stormed off, she washed off in the bathroom.
+pause 
+cls
+echo Tammy: Stupid! Stupid! 
+pause
+cls
+echo At times like this, she blames herself 
+echo For not sticking up to them more.
+pause 
+cls
+echo She headed to class but zoned out during the lecture.
+pause 
+cls
+Color 02
+echo Tammy *Thinking*: My birthday is pretty much ruined
+echo I smell like milk and look even worse.
+pause 
+cls
+echo  *Thinking*: Why even bother with these classes,
+echo Honestly I could just drop out. 
+echo My parents wealth could support me till the day I die.
+pause 
+cls
+echo *Thinking*: Its not like I have any friends
+echo Or a cute guy to look at in class either.
+pause 
+cls
+echo Tammy contemplated life for a moment, school, family
+echo What good is it all?
+pause 
+cls
+Color 08
+echo She took the opportunity to stare out the window.
+pause 
+cls
+echo She had seen many sights in her life
+echo But this particular view calms her more than anything.
+pause 
+cls
+echo There laid a beautiful pond where kids fed the ducks.
+echo People frequently visit it due to its calm atmosphere. 
+echo Large trees usually hid the park but 
+echo Being on the 3rd floor helped her see things
+echo She could have never seen anywhere else.
+pause 
+cls
+echo The forest stretched on for a while it seemed.
+echo She wondered how long it would take to jog around the area
+echo Though she has never been the athletic type.
+pause 
+cls
+echo She stared at the center of the forest
+echo The trees covered most of the ground,
+echo But there was a small piece of land where the sun shone.
+pause 
+cls
+echo Looking at it even closer she began to see things unfold.
+pause 
+cls
+echo A fire, a fire that began to grow and grow
+echo Trees falling down and the city going into panic.
+pause 
+cls
+echo She had no time to react when she realized 
+echo What was unfolding was real.
+pause 
+cls
+echo As quick as it began, a small figure in the fire released
+echo A spark of light. A giant explosion occured shaking 
+echo The entire building.
+pause 
+cls
+echo There was no time to stare anymore
+echo She fell out of her seat and began to yell-
+pause 
+cls
+echo Tammy: What is that!? A nuke? 
+echo We need to get to safety and-
+pause
+cls
+echo She opened her eyes and stood from the floor. 
+echo The students looked at each other, clueless as 
+echo To why she was screaming.
+pause 
+cls
+echo Tammy had realized, what had just occured was fake somehow.
+echo The outside is the same as it had been a few minutes ago.
+echo But the destruction so real she could feel the shaking
+echo From the classroom.
+pause 
+cls
+echo Student: Pfft-
+pause 
+cls
+echo Everyone began laughing so hard they couldnt contain themselves.
+pause 
+cls
+echo Male Student: What kind of sci-fi movies have you been watching!?
+pause 
+cls
+echo Tammy: What?! I saw what I saw! Did anyone else see that explosion?!
+pause 
+cls
+echo Female Student: *looking at phone* That explosion
+echo was a 4.6 magnitude earthquake.
+pause 
+cls
+echo Tammy: Huhh!?!
+pause 
+cls
+echo Professor: Were no stranger to earthquakes. Settle down.
+pause 
+cls
+echo Tears began forming, she saw something no one else had.
+echo Or maybe they just werent looking. 
+echo Once again she had become the laughing stock for everyone.
+pause 
+cls
+echo The bell rung and she stormed out of class.
+pause 
+cls
+echo Going through the hall was worse than she wouldve expected.
+pause 
+cls
+echo Male Student: And this girl said- a nuke!
+pause 
+cls
+echo Male Student 2: No way, seriously!
+pause 
+cls
+echo Female Student: But how on Earth can you mistake the two?
+pause 
+cls
+echo Female Student 2: People are saying she suffers from some
+echo Kind of illness, like, with her brain!
+pause 
+cls
+echo She pushed her way out of the school and
+echo began to walk to the forest
+pause 
+cls
+echo Tammy: If they dont believe me, then I just have to prove it!
+pause 
+cls
+echo Suddenly she felt a jolt in her body.
+pause 
+cls
+Color 02
+echo Tammy *Thinking*: Theres no doubt what I saw was real, but-
+pause 
+cls
+echo Tammy *Thinking*: Wouldnt going there, subject me to the
+echo Same danger I saw? 
+pause 
+cls
+echo She stopped infront of the forest and thought a while about it.
+pause 
+cls
+echo Tammy *Thinking*: Well, I made up my mind. If I'm going to do 
+echo It, I have to be back in time for the Cafe!
+pause 
+cls
+Color 08
+echo Tammy: Alright! Let's do this!
+pause 
+cls
+echo
+pause 
+cls
+echo
+pause 
+
+
+
+
